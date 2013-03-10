6d0d0e4fa31dd02defb109b94fa4d8831d953a22
j3group.s.a <j3group.s.a@gmail.com>
2013-03-10 02:33:46 +0000

    Actualizacion de modelo pla/_view.php pla/admin.php pla/create.php pla/view.php todos los referentes a Plaza de odontologia

---  odsys1/protected/views/pla/admin.php
+++  odsys1/protected/views/pla/admin.php
@@ -3,13 +3,13 @@
 /* @var $model Pla */
 
 $this->breadcrumbs=array(
-	'Plas'=>array('index'),
-	'Manage',
+	'Plazas'=>array('index'),
+	'Administrar',
 );
 
 $this->menu=array(
-	array('label'=>'List Pla', 'url'=>array('index')),
-	array('label'=>'Create Pla', 'url'=>array('create')),
+	array('label'=>'Listar Plazas', 'url'=>array('index')),
+	array('label'=>'Nueva Plaza', 'url'=>array('create')),
 );
 
 Yii::app()->clientScript->registerScript('search', "
@@ -26,14 +26,14 @@ $('.search-form form').submit(function(){
 ");
 ?>
 
-<h1>Manage Plas</h1>
+<h1>Administrar Plazas del servicio</h1>
 
 <p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
+Si lo desea, puede introducir un operador de comparación (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
+o <b>=</b>)al comienzo de cada uno de los valores de su búsqueda para especificar cómo la comparación se debe hacer.
 </p>
 
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<?php echo CHtml::link('Busqueda avanzada','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
 <?php $this->renderPartial('_search',array(
 	'model'=>$model,
---  odsys1/protected/views/pla/view.php
+++  odsys1/protected/views/pla/view.php
@@ -3,20 +3,20 @@
 /* @var $model Pla */
 
 $this->breadcrumbs=array(
-	'Plas'=>array('index'),
+	'Plaza'=>array('index'),
 	$model->PLA,
 );
 
 $this->menu=array(
-	array('label'=>'List Pla', 'url'=>array('index')),
-	array('label'=>'Create Pla', 'url'=>array('create')),
-	array('label'=>'Update Pla', 'url'=>array('update', 'id'=>$model->PLA)),
-	array('label'=>'Delete Pla', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->PLA),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Pla', 'url'=>array('admin')),
+	array('label'=>'Listar Plazas', 'url'=>array('index')),
+	array('label'=>'Nueva Plaza', 'url'=>array('create')),
+	array('label'=>'Actualizar Plaza', 'url'=>array('update', 'id'=>$model->PLA)),
+	array('label'=>'Eliminar Plaza', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->PLA),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Administrar', 'url'=>array('admin')),
 );
 ?>
 
-<h1>View Pla #<?php echo $model->PLA; ?></h1>
+<h1>Plaza : <?php echo $model->PLA; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
