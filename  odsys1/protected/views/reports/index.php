fe301e3a287e5289d9dd8c04195dee29c3d146ec
j3group.s.a <j3group.s.a@gmail.com>
2013-04-27 21:02:17 +0000

    Modificaciones de la interface de reportes.

---  odsys1/protected/views/reports/index.php
+++  odsys1/protected/views/reports/index.php
@@ -11,8 +11,17 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 <h1><?php echo CHtml::encode(Yii::app()->name); ?> Reportes</h1>
 
+<div class="flash-notice">Click sobre el reporte que desea obtener.</div>
+    
     <div class="dashIcon span-3">
         <a href="/odsys/index.php/reports/report1"><img src="<?php echo Yii::app()->theme->baseUrl; ?>/images/big_icons/icon-chart.png" alt="Page" /></a>
-        <div class="dashIconText"><a href="#1"><?php echo CHtml::link('Reporte 1',array('report1'))?></a></div>
+        <div class="dashIconText"><a href="#1"><?php echo CHtml::link('Reporte Consultas',array('report1'))?></a></div>
     </div>
-</div>
+    <div class="dashIcon span-3">
+        <a href="/odsys/index.php/pers/index"><img src="<?php echo Yii::app()->theme->baseUrl;?>/images/big_icons/icon-contact2.png" alt="Order History" /></a>
+        <div class="dashIconText"><a href="/odsys/index.php/pers/index">Personas</a></div>
+    </div>
+    <div class="dashIcon span-3">
+        <a href="/odsys/index.php/pers/index"><img src="<?php echo Yii::app()->theme->baseUrl;?>/images/big_icons/icon-contact2.png" alt="Order History" /></a>
+        <div class="dashIconText"><a href="/odsys/index.php/pers/index">Unidades programaticas</a></div>
+    </div>
