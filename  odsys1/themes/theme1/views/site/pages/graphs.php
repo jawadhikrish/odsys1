aa4aefecc4953f5d76fcc9cad49d47aff506409b
j3group.s.a <j3group.s.a@gmail.com>
2013-06-09 02:02:22 +0000

    se añade libreria para widget de los graficos, ya que los que estan son estatico en html. de igual menara se pueden usar sin embargo considero mejor usar widgets

---  odsys1/themes/theme1/views/site/pages/graphs.php
+++  odsys1/themes/theme1/views/site/pages/graphs.php
@@ -321,6 +321,196 @@ $this->beginWidget('zii.widgets.CPortlet', array(
 </div>
 <?php $this->endWidget();?>
 
+<?php 
+        $this->widget(
+            'chartjs.widgets.ChBars', 
+            array(
+                'width' => 600,
+                'height' => 300,
+                'htmlOptions' => array(),
+                'labels' => array("January","February","March","April","May","June"),
+                'datasets' => array(
+                    array(
+                        "fillColor" => "#ff00ff",
+                        "strokeColor" => "rgba(220,220,220,1)",
+                        "data" => array(10, 20, 30, 40, 50, 60)
+                    )       
+                ),
+                'options' => array()
+            )
+        ); 
+    ?>
+Line Chart
+
+<?php 
+        $this->widget(
+            'chartjs.widgets.ChLine', 
+            array(
+                'width' => 600,
+                'height' => 300,
+                'htmlOptions' => array(),
+                'labels' => array("January","February","March","April","May","June"),
+                'datasets' => array(
+                    array(
+                        "fillColor" => "rgba(220,220,220,0.5)",
+                        "strokeColor" => "rgba(220,220,220,1)",
+                        "pointColor" => "rgba(220,220,220,1)",
+                        "pointStrokeColor" => "#ffffff",
+                        "data" => array(10, 20, 25, 25, 50, 60)
+                    ),
+                    array(
+                        "fillColor" => "rgba(220,220,220,0.5)",
+                        "strokeColor" => "rgba(220,220,220,1)",
+                        "pointColor" => "rgba(220,220,220,1)",
+                        "pointStrokeColor" => "#ffffff",
+                        "data" => array(55, 50, 45, 30, 20, 10)
+                    )      
+                ),
+                'options' => array()
+            )
+        ); 
+    ?>
+Radar Chart
+
+<?php 
+        $this->widget(
+            'chartjs.widgets.ChRadar', 
+            array(
+                'width' => 600,
+                'height' => 300,
+                'htmlOptions' => array(),
+                'labels' => array("January","February","March","April", "May"),
+                'datasets' => array(
+                    array(
+                        "fillColor" => "rgba(220,220,220,0.5)",
+                        "strokeColor" => "rgba(220,220,220,1)",
+                        "pointColor" => "rgba(220,220,220,1)",
+                        "pointStrokeColor" => "#ffffff",
+                        "data" => array(5, 15, 50, 25, 35)
+                    ),
+                    array(
+                        "fillColor" => "rgba(220,220,220,0.5)",
+                        "strokeColor" => "rgba(220,220,220,1)",
+                        "pointColor" => "rgba(220,220,220,1)",
+                        "pointStrokeColor" => "#ffffff",
+                        "data" => array(55, 50, 45, 30, 20)
+                    )      
+                ),
+                'options' => array()
+            )
+        ); 
+    ?>
+Polar Area Chart
+
+<?php 
+        $this->widget(
+            'chartjs.widgets.ChPolar', 
+            array(
+                'width' => 600,
+                'height' => 300,
+                'htmlOptions' => array(),
+                'drawLabels' => true,
+                'datasets' => array(
+                    array(
+                        "value" => 50,
+                        "color" => "rgba(220,30, 70,1)",
+                        "label" => "Hunde"
+                    ),
+                    array(
+                        "value" => 25,
+                        "color" => "rgba(66,66,66,1)",
+                        "label" => "Katzen"
+                    ),
+                    array(
+                        "value" => 40,
+                        "color" => "rgba(100,100,220,1)",
+                        "label" => "Vögel"
+                    ),
+                    array(
+                        "value" => 15,
+                        "color" => "rgba(20,120,120,1)",
+                        "label" => "Mäuse"
+                    )
+                ),
+                'options' => array()
+            )
+        ); 
+    ?>
+Pie Chart
+
+<?php 
+            $this->widget(
+                'chartjs.widgets.ChPie', 
+                array(
+                    'width' => 600,
+                    'height' => 300,
+                    'htmlOptions' => array(),
+                    'drawLabels' => true,
+                    'datasets' => array(
+                        array(
+                            "value" => 50,
+                            "color" => "rgba(220,30, 70,1)",
+                            "label" => "Hunde"
+                        ),
+                        array(
+                            "value" => 25,
+                            "color" => "rgba(66,66,66,1)",
+                            "label" => "Katzen"
+                        ),
+                        array(
+                            "value" => 40,
+                            "color" => "rgba(100,100,220,1)",
+                            "label" => "Vögel"
+                        ),
+                        array(
+                            "value" => 15,
+                            "color" => "rgba(20,120,120,1)",
+                            "label" => "Mäuse"
+                        )
+                    ),
+                    'options' => array()
+                )
+            ); 
+        ?>
+Doughnut Chart
+
+<?php 
+        $this->widget(
+            'chartjs.widgets.ChDoughnut', 
+            array(
+                'width' => 600,
+                'height' => 300,
+                'htmlOptions' => array(),
+                'drawLabels' => true,
+                'datasets' => array(
+                    array(
+                        "value" => 50,
+                        "color" => "rgba(220,30, 70,1)",
+                        "label" => "Hunde"
+                    ),
+                    array(
+                        "value" => 25,
+                        "color" => "rgba(66,66,66,1)",
+                        "label" => "Katzen"
+                    ),
+                    array(
+                        "value" => 40,
+                        "color" => "rgba(100,100,220,1)",
+                        "label" => "Vögel"
+                    ),
+                    array(
+                        "value" => 15,
+                        "color" => "rgba(20,120,120,1)",
+                        "label" => "Mäuse"
+                    )
+                ),
+                'options' => array()
+            )
+        ); 
+    ?>
+
+
+
 <div class="flash-notice">
 <p>This is a "static" page. You may change the content of this page
 by updating the file <tt><?php echo __FILE__; ?></tt>.</p>
